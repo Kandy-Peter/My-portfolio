c178ff4402d658d6588533f478636dd1f03a19dd
Kandy Peter <kandypeter03@gmail.com>
2021-12-14 16:27:23 +0200

    Add styled menu and the closeMenu function

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const navigation = document.querySelector('.toggle');
   navigation.addEventListener('click', function() {
   body.classList.toggle('open-menu');
  });
- 
+
+const menuLink = document.querySelectorAll('.menu-link');
+
+menuLink.forEach(e => e.addEventListener("click", function(){
+    body.classList.remove('open-menu');
+}));
 
