e010849a3202276ba1cbba9e0d43c69444d25849
asanchy <sanchyclips@gmail.com>
2021-12-14 11:33:24 +0200

    Add JS file and event Listener

---
+++ app.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+let navigation = document.querySelector('.toggle');
+
+navigation.addEventListener('click', function() {
+body.classList.toggle('open-menu');
+ });
+ 
+
