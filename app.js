e85a474d093d575f02f51fcf18f8913557036616
Kandy Peter <kandypeter03@gmail.com>
2021-12-14 17:48:43 +0200

    Fix Linters

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const body = document.querySelector('body');
 const navigation = document.querySelector('.toggle');
 
-  navigation.addEventListener('click', function() {
+navigation.addEventListener('click', () => {
   body.classList.toggle('open-menu');
- });
+});
 
 const menuLink = document.querySelectorAll('.menu-link');
 
-menuLink.forEach(e => e.addEventListener("click", function(){
-    body.classList.remove('open-menu');
+menuLink.forEach((e) => e.addEventListener('click', () => {
+  body.classList.remove('open-menu');
 }));
-
